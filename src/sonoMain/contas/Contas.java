1e581c585660fe05a39a8e77bc4e1e2fffe85202
Luis Fernando Guerreiro <luisfg30@hotmail.com>
2015-11-03 14:05:32 -0200

    ok

---
+++ src/sonoMain/contas/Contas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sonoMain.contas;
+
+/**
+ *
+ * @author luisfg30
+ */
+public class Contas {
+    
+    
+    public void calcCaracteristicas(int numeroAmostras,int freqAmostragem){     
+        FFT fft = new FFT(numeroAmostras, freqAmostragem);
+        
+    }
+    
+    
+}
